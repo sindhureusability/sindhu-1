201c864723688b837d3d02832631aed5b623be26
sindhu-9908 <155870295+sindhu-9908@users.noreply.github.com>
2024-01-09 22:52:15 +0530

    firstcommit2

---
+++ src/practicepack__/PGI.java
@@ -0,0 +1,17 @@
+package practicepack__;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PGI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        WebDriver driver = new ChromeDriver();
+        int i=10;
+        System.out.println(i);
+        
+        		
+	}
+
+}
