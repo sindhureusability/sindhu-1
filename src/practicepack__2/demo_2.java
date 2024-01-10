31342a94fa7e575cfdfc98118b8cb4b7e198c31d
sindhu-9908 <155870295+sindhu-9908@users.noreply.github.com>
2024-01-10 11:10:35 +0530

    firstcommit2

---
+++ src/practicepack__2/demo_2.java
@@ -0,0 +1,17 @@
+package practicepack__2;
+
+import java.util.Scanner;
+
+public class demo_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int i=0;
+     int j=i;
+     j=i+++j;
+     System.out.println(i+"-"+j);
+    // int num=sc.nextInt();//1234
+     
+	}
+
+}
---
+++ src/practicepack__2/package-info.java
@@ -0,0 +1,1 @@
+package practicepack__2;
